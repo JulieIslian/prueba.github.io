8a0238a7831ec7da3174a348f410d38e8336278f
JulieIslian <129077303+JulieIslian@users.noreply.github.com>
2023-04-24 09:27:40 -0500

    main

---
+++ JS/operador.js
@@ -0,0 +1,7 @@
+
+    //Conversion Larga
+    var celsius1 = prompt ("introduzca la temperatura en grados celsius: ");
+    celsius1 = parseFloat(celsius1); 
+    //Hacer Conversion de Celsius a Farenheit :
+    var Farenheit1 = 9/5*celsius1+32;
+    document.write("De Celsius a Farenheit es: " Farenheit1);
